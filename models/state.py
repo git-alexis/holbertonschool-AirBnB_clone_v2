92c8fce4ca4a0790aa40c89155f0be4223378464
Diesos <ozturk@live.Fr>
2024-03-30 12:54:53 +0100

    Refactor City and State models

--- models/state.py
+++ models/state.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
-class State(BaseModel):
+class State(BaseModel, Base):
 	""" State class """
+	__tablename__ = 'states'
+
+	name = Column(String(128), nullable=False)
+
 	if getenv('HBNB_TYPE_STORAGE') == 'db':
-		__tablename__ = 'states'
 		name = Column(String(128), nullable=False)
 		cities = relationship("City", backref="state")
 	else:
@@ -17,11 +22,8 @@ class State(BaseModel):
 	@property
 	def cities(self):
 		"""getter attribute cities that returns the list of City instances"""
-		from models import storage
-		from models.city import City
-		cities = storage.all(City)
-		city_list = []
-		for city in cities.values():
+		cities_list = []
+		for city in models.storage.all(City).values():
 			if city.state_id == self.id:
-				city_list.append(city)
-		return city_list
+				cities_list.append(city)
+		return cities_list
