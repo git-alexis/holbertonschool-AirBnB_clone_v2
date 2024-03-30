af50331231f1ca4c85b669878d1288859932003f
Diesos <ozturk@live.Fr>
2024-03-30 12:58:49 +0100

    Refactor DBStorage class and fix typo in getenv() function call

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
+from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv
 from models.base_model import Base
 from models.state import State
 from models.city import City
 from models.user import User
 from models.place import Place
 
-__engine = None
-__session = None
+class DBStorage():
+    """ Class to manage storage of hbnb models in MySQL database """
+    __engine = None
+    __session = None
 
-def __init__(self):
-	""" Initialize the database engine """
-	self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-		getenv('HBNB_MYSQL_USER'),
-		getenv('HBNB_MYSQL_PWD'),
-		getenv('HBNB_MYSQL_HOST'),
-		getenv('HBNB_MYSQL_DB'), 
-		pool_pre_ping=True
-	))
-	
-	if gentenv('HBNB_ENV') == 'test':
-		Base.metadata.drop_all(self.__engine)
+	def __init__(self):
+		""" Initialize the database engine """
+		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+			getenv('HBNB_MYSQL_USER'),
+			getenv('HBNB_MYSQL_PWD'),
+			getenv('HBNB_MYSQL_HOST'),
+			getenv('HBNB_MYSQL_DB'), 
+			pool_pre_ping=True
+		))
+		
+		if gentenv('HBNB_ENV') == 'test':
+			Base.metadata.drop_all(self.__engine)
 
-		def all(self, cls=None):
-		"""Queries the database based on the class name"""
-		obj_dict = {}
+	def all(self, cls=None):
+	"""Queries the database based on the class name"""
+	obj_dict = {}
 
-		if cls:
-			query = self.__session.query(cls)
-		else:
-			query = self.__session.query(State, City, User, Place)
+	if cls:
+		query = self.__session.query(cls)
+	else:
+		query = self.__session.query(State, City, User, Place)
 
-		for obj in query:
-			key = "{}.{}".format(obj.__class__.__name__, obj.id)
-			obj_dict[key] = obj
+	for obj in query:
+		key = "{}.{}".format(obj.__class__.__name__, obj.id)
+		obj_dict[key] = obj
 
-		return obj_dict
+	return obj_dict
 
 	def new(self, obj):
 		"""Adds a new object to the database session"""
