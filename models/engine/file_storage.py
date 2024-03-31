43b033b7bc2280a26cd92eb9a00c3cdcb87164eb
git-alexis <alex59.billemont@gmail.com>
2024-03-31 23:15:11 +0200

    fix formatting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,64 +4,62 @@ import json
 
 
 class FileStorage:
-	"""This class manages storage of hbnb models in JSON format"""
-	__file_path = 'file.json'
-	__objects = {}
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
 
-		def all(self, cls=None):
-			"""Returns a dictionary of models currently in storage"""
-			if cls is not None:
-				return {key: obj for key, obj in FileStorage.__objects.items()
-						if isinstance(obj, cls)}
-			else:
-				return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            temp = {}
+            for key, val in self.__objects.items():
+                if cls.__name__ in key:
+                    temp[key] = val
+            return temp
+        else:
+            return self.__objects
 
-		def new(self, obj):
-			"""Adds new object to storage dictionary"""
-			self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
-		def save(self):
-			"""Saves storage dictionary to file"""
-			with open(FileStorage.__file_path, 'w') as f:
-				temp = {}
-				temp.update(FileStorage.__objects)
-				for key, val in temp.items():
-					temp[key] = val.to_dict()
-				json.dump(temp, f)
+    def save(self):
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
-		def reload(self):
-			"""Loads storage dictionary from file"""
-			from models.base_model import BaseModel
-			from models.user import User
-			from models.place import Place
-			from models.state import State
-			from models.city import City
-			from models.amenity import Amenity
-			from models.review import Review
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-			classes = {
-						'BaseModel': BaseModel, 'User': User, 'Place': Place,
-						'State': State, 'City': City, 'Amenity': Amenity,
-						'Review': Review
-					}
-			try:
-				temp = {}
-				with open(FileStorage.__file_path, 'r') as f:
-					temp = json.load(f)
-					for key, val in temp.items():
-						self.all()[key] = classes[val['__class__']](**val)
-			except FileNotFoundError:
-				pass
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
+        except FileNotFoundError:
+            pass
 
-		def delete(self, obj=None):
-			""" delete an objecrt on storage """
-			if obj is None:
-				return
-			else:
-				key = obj.to_dict()['__class__'] + '.' + obj.id
-				if key in FileStorage.__objects:
-					del FileStorage.__objects[key]
-
-		def close(self):
-			""" Deserialize JSON file to objects before leaving """
-			self.reload()
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+            self.save()
